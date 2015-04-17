4e794598acfc3143b31e5de04f7489478d9d4ed1
海峰 黄 <wonghoifung@haifengdeMacBook-Air.local>
2015-04-17 23:37:03 +0800

    test callback

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 #include "eventloop.h"
 #include "tcpserver.h"
 
+void on_inpack1(connection* conn, inpack1* pack) {
+}
+
+void on_connected(connection* conn) {
+}
+
+void on_closed(connection* conn) {
+}
+
 int main( int argc, char* argv[] )
 {
     if( argc <= 2 )
@@ -16,6 +25,9 @@ int main( int argc, char* argv[] )
 
     event_loop loop;
     tcpserver tsvr(&loop);
+    tsvr.set_inpack1callback(on_inpack1);
+    tsvr.set_connectedcallback(on_connected);
+    tsvr.set_closedcallback(on_closed);
     tsvr.init(port);
     loop.run();
 
